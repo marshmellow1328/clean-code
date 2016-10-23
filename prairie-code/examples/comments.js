3d722e1258ad791139ded6c9a484589eab6c6c49
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2016-10-23 15:17:14 -0500

    updated comments slides

--- prairie-code/examples/comments.js
+++
@@ -1,8 +0,0 @@
-// add attendee
-talk.addAttendee( 'Shawn' );
-
-
-// add domains
-template.add( 'google', 'https://www.google.com' );
-template.add( 'yahoo', 'https://www.yahoo.com' );
-template.add( 'phone', '555-5555' );
